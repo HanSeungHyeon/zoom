5cea5f713cdfcc4c5ad398eb8a9be3e3f403d95d
hsh <hsh@curioud.com>
2025-02-03 17:19:24 +0900

    소켓 - 아이디추가가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,49 @@
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nicknameForm = document.querySelector('#nickname');
+var nickname = '';
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+
+socket.addEventListener('open', () => {
+  console.log('Connected to browser');
+})
+
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li')
+  const msg = JSON.parse(message.data)
+  li.innerHTML = msg.message
+  console.log(msg)
+  messageList.append(li)
+})
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected to server')
+})
+
+// setTimeout(() => {
+//   socket.send("hello from the browser")
+// }, 3000)
+const makeMessage = (type, user, message) => {
+  const msg = { type, user, message }
+  return JSON.stringify(msg)
+}
+
+const handleMessageSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  if(!!!nickname) return
+  socket.send(makeMessage('message', nickname, input.value))
+  input.value = ''
+}
+
+const handleNicknameSubmit = (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector('input')
+  socket.send(makeMessage('nickname', nickname, input.value))
+  nickname = input.value
+  input.disabled = true
+}
+
+messageForm.addEventListener('submit', handleMessageSubmit);
+nicknameForm.addEventListener('submit', handleNicknameSubmit);
--- src/server.js
+++ src/server.js
@@ -16,9 +16,47 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket)
-};
-wss.on("connection", handleConnection)
+const makeMessge = (user, message) => {
+  const msg = { user, message }
+  return JSON.stringify(msg)
+}
+// 1. 소켓 연결 끊겼을 경우
+const onSocketClose = () => {
+  console.log('Disconnected from the browser')
+  sockets.pop()
+}
+
+// 2. 브라우저 -> 서버 -> 브라우저
+const onSocketMessage = (message) => {
+  const jMsg = JSON.parse(message)
+
+  switch (jMsg.type){
+    case 'message':
+      sockets.forEach(socket => {
+        socket.send(makeMessge(jMsg.message))
+      })
+    case 'nickname':
+      // socket['nickname'] = jMsg.message
+      console.log(this)
+      // this.socket['nickname'] = jMsg.message
+
+  }
+}
+
+//3. 서버 -> 브라우저 메세지
+// const onServerMessage = (socket, message = 'hello') => {
+//   socket.send(message)
+// }
+
+//소켓 리스트
+const sockets = [];
+
+
+//소켓 연결 시
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket.on('close', onSocketClose) //1
+  socket.on('message', onSocketMessage) //2
+})
 
 server.listen(3000, handleListen)
